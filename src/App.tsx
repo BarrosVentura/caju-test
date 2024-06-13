63fa09415cc96b8d6005b5d2dcbc99c34bb1af10
Adson Ventura <adsonventura@gmail.com>
2024-06-13 14:52:28 -0300

    feat: add libs for usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Router from "~/router";
 import Header from "./components/Header";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header>
         <h1>Caju Front Teste</h1>
       </Header>
       <Router />
-    </>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
